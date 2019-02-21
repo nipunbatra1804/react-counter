d7b10012790bbbbd34a68f2c00c8e285574d3256
nipunbatra1994@gmail.com <nipunbatra1994@gmail.com>
2019-02-21 11:18:53 +0800

    Initial Commit - Part 2

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -4,12 +4,40 @@ export class Counter extends Component {
   state = { id: 1, value: 0 };
 
   render() {
+    const style = { width: "50px" };
     return (
-      <h2>
-        <span className="badge badge-primary">{this.state.value}</span>
-      </h2>
+      <React.Fragment>
+        <h2>
+          <span className="badge badge-primary" style={style}>
+            {this.state.value}{" "}
+          </span>{" "}
+          <button
+            onClick={this.handleClickIncrement}
+            type="button"
+            className="btn btn-success"
+          >
+            +
+          </button>
+          <button
+            onClick={this.handleClickDecrement}
+            type="button"
+            className="btn btn-danger"
+          >
+            -
+          </button>
+        </h2>
+      </React.Fragment>
     );
   }
+
+  handleClickIncrement = () => {
+    const value = this.state.value + 1;
+    this.setState({ value: value });
+  };
+  handleClickDecrement = () => {
+    const value = this.state.value - 1;
+    this.setState({ value: value });
+  };
 }
 
 export default Counter;
