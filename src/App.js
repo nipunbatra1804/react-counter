4cb32aba90c5ccda81c240052ce374ecd257b3c9
nipunbatra1994@gmail.com <nipunbatra1994@gmail.com>
2019-02-21 10:49:33 +0800

    Initial Commit - Part 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Counter from "./components/Counter/Counter";
 
 export class App extends Component {
   render() {
     return (
-      <h1>
-        Counter Lab
-      </h1>
-    )
+      <React.Fragment>
+        <h1>Counter lab</h1> <Counter />
+      </React.Fragment>
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class Counter extends Component {
+  state = { id: 1, value: 0 };
+
+  render() {
+    return (
+      <h2>
+        <span className="badge badge-primary">{this.state.value}</span>
+      </h2>
+    );
+  }
+}
+
+export default Counter;
