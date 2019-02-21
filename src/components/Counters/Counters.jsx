2b0179b711b120318b5e4d1c6e1aaec138b8e098
nipunbatra1994@gmail.com <nipunbatra1994@gmail.com>
2019-02-21 14:27:15 +0800

    Counters - Part 7

--- src/components/Counters/Counters.jsx
+++ src/components/Counters/Counters.jsx
@@ -6,14 +6,24 @@ export class Counters extends Component {
   state = { id: 0, data: counters };
   render() {
     const counters = this.state.data;
-    const htmlElement = counters.map(element => (
-      <Counter
-        key={element.id}
-        id={element.id}
-        value={element.value}
-        handleClickDelete={() => this.handleClickDelete(element)}
-      />
-    ));
+    const noCounterComp = (
+      <h2>
+        <span className={`badge badge-primary`}>N0 COUNTERS LEFT</span>
+      </h2>
+    );
+
+    const htmlElement =
+      counters.length !== 0
+        ? counters.map(element => (
+            <Counter
+              key={element.id}
+              id={element.id}
+              value={element.value}
+              handleClickDelete={() => this.handleClickDelete(element)}
+            />
+          ))
+        : noCounterComp;
+
     return <div>{htmlElement}</div>;
   }
   handleClickDelete = counter => {
